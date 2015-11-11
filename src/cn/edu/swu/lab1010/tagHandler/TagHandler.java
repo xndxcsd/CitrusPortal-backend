3e6ac019d549c90f2f4e1e9b842b506539e4f738
Xndxcsd <chensiding@qq.com>
2015-11-11 13:36:58 +0800

    rename package

--- src/cn/edu/swu/lab1010/tagHandler/TagHandler.java
+++ src/cn/edu/swu/lab1010/tagHandler/TagHandler.java
@@ -1,9 +1,9 @@
-package cn.swu1010.tagHandler;
+package cn.edu.swu.lab1010.tagHandler;
 
 
 /**
  *  这个接口规定了部分的标签操作。实现了这个接口的
- * 	类具有读入RDF标间
+ * 	类具有对RDF标记文件解析、
  * @author Siding Chen
  * @version 2015年11月11日 下午1:14:20
  */
