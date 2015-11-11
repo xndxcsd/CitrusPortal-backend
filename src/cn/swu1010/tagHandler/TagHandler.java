48043c9464acc3b3ed9519aaef0a85f61bdc5366
Xndxcsd <chensiding@qq.com>
2015-11-11 13:23:00 +0800

    test new eclipse

---
+++ src/cn/swu1010/tagHandler/TagHandler.java
@@ -0,0 +1,16 @@
+package cn.swu1010.tagHandler;
+
+
+/**
+ *  这个接口规定了部分的标签操作。实现了这个接口的
+ * 	类具有读入RDF标间
+ * @author Siding Chen
+ * @version 2015年11月11日 下午1:14:20
+ */
+
+public interface TagHandler {
+
+	
+	
+	
+}
