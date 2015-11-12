74401a52b583868001858747a5101c77179e8f5a
Xndxcsd <chensiding@qq.com>
2015-11-12 20:09:42 +0800

    read file to Model ; change NodeIterator to HashSet<String>; //TO-DO //get the resultModel

--- src/cn/edu/swu/lab1010/tagHandler/RdfDao.java
+++ src/cn/edu/swu/lab1010/tagHandler/RdfDao.java
@@ -1,11 +1,18 @@
 package cn.edu.swu.lab1010.tagHandler;
 
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 
-import org.apache.jena.rdf.model.*;
-import org.apache.jena.util.*;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.NodeIterator;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.ResIterator;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.SimpleSelector;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.util.FileManager;
 import org.apache.jena.vocabulary.RDFS;
 
 /**
@@ -13,25 +20,23 @@ import org.apache.jena.vocabulary.RDFS;
  */
 public class RdfDao {
 
-	/**
-	 * read the rdf to a jena Model .
-	 * 
-	 * @return Model
-	 */
-	public final Model readTagRdf(String filePath) throws Exception {
-		/*
-		 * make the rdf to a jena model.TAGFILEPATH is a temporary filepath for
-		 * test.
-		 */
+	Model model;
 
-		Model model = ModelFactory.createDefaultModel();
+	/**
+	 * 传入文件路径，将该rdf文件读入jena model中，以便后续的操作。
+	 * 
+	 * @param filePath
+	 */
+	public RdfDao(String filePath) {
+		// TODO Auto-generated constructor stub
+
+		this.model = ModelFactory.createDefaultModel();
 
 		InputStream in = FileManager.get().open(filePath);
 		if (null == in) {
 			throw new IllegalArgumentException("File :" + filePath + "not found");
 		}
 		model.read(in, null);
-		return model;
 	}
 
 	/**
@@ -40,9 +45,9 @@ public class RdfDao {
 	 * @param model
 	 * @return NodeIterator
 	 */
-	public final NodeIterator listObjectWithlabel(Model model) {
+	public final HashSet<String> listObjectWithlabel() {
 
-		if (model.isEmpty()) {
+		if (this.model.isEmpty()) {
 			throw new IllegalArgumentException("" + model + "is not exist");
 		}
 
@@ -68,10 +73,39 @@ public class RdfDao {
 		//
 		// }
 
-		return iter;
+		// 将NodeIterator中的结点遍历并转换到规则集（set）中
+		HashSet<String> labelSet = new HashSet<>();
+		while (iter.hasNext()) {
+			RDFNode node = iter.nextNode();
+			labelSet.add(node.toString());
+		}
+		// 返回该规则集
+		return labelSet;
 	}
 
-	
-	
-	
+	/**
+	 * 传入匹配成功的label，返回由其对应的资源和其前驱，前驱的前驱，后继组成的jena Model
+	 * @param label
+	 */
+	public final void searchByLabel(String label) {
+		//resultModel用来存放查找的结果，这里我们将查找的结果重新构造成一张图
+		Model resultModel = ModelFactory.createDefaultModel();
+		//返回一个属性值为label的所有属性的迭代器
+		ResIterator resIter = model.listResourcesWithProperty(RDFS.label, label);
+		//将这个迭代器中的所有资源添加到resultModel中
+		while (resIter.hasNext()){
+			Resource res = resIter.next();
+			
+		}
+		
+	}
+
+	/**
+	 * 返回该对象处理的模型
+	 * 
+	 * @return (jena.)Model
+	 */
+	public Model getModel() {
+		return model;
+	}
 }
--- src/cn/edu/swu/lab1010/tagHandler/Test.java
+++ src/cn/edu/swu/lab1010/tagHandler/Test.java
@@ -8,13 +8,11 @@ public class Test {
 	private static final String TAGFILEPATH = "C:\\Users\\Alexi\\Desktop" + "\\陈思定用工具\\CitrusTest1.3_modified.rdf";
 
 	public static void main(String[] args) {
-		RdfDao rdfdao = new RdfDao();
+		RdfDao rdfdao = new RdfDao(TAGFILEPATH);
 		Model model = null;
 		try {
 			//将储存着标签的rdf读到model中
-			model = rdfdao.readTagRdf(TAGFILEPATH);
 			//得到迭代标签的迭代器，测试代码在方法体中
-			rdfdao.listObjectWithlabel(model);
 			//
 			
 		} catch (Exception e) {
