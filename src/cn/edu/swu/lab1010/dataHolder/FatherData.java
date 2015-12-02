e7583421a0ad29174c96eb660f0e22ab0589f43b
xndxcsd <985857416@qq.com>
2015-12-02 20:13:35 +0800

    。

---
+++ src/cn/edu/swu/lab1010/dataHolder/FatherData.java
@@ -0,0 +1,18 @@
+package cn.edu.swu.lab1010.dataHolder;
+
+public class FatherData extends Data {
+    public FatherData(String URI, String label) {
+        super(URI, label);
+
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int getRelation() {
+        // TODO Auto-generated method stub
+        return 1;
+    }
+    
+    
+    
+}
